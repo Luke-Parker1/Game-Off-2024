da9a5c5827ed18466c13613caedd357943d55726
Luke Parker <Luke.Parker1001@gmail.com>
2024-11-23 20:33:03 -0500

    Added dash ability and reworked some code

--- main.gd
+++ main.gd
@@ -8,7 +8,8 @@ var aware_volume : int
 var person_group : Array
 
 func _ready():
-	$MindEraseBar.max_value = $Player.flash_cooldown
+	$MindEraseBar.max_value = $Player/MindWipeCooldown.wait_time
+	$DashBar.max_value = $Player/DashCooldown.wait_time
 	
 	person_group = get_tree().get_nodes_in_group("Person").duplicate()
 	aware_volume = person_group.size() + 1
@@ -25,4 +26,5 @@ func _ready():
 	
 
 func _process(delta):
-	$MindEraseBar.value = $Player.active_flash_cooldown
+	$MindEraseBar.value = $Player/MindWipeCooldown.wait_time - $Player/MindWipeCooldown.time_left
+	$DashBar.value = $Player/DashCooldown.wait_time - $Player/DashCooldown.time_left
--- player.gd
+++ player.gd
@@ -2,14 +2,24 @@ extends CharacterBody2D
 
 @export var speed := 60.0
 
-# Time in seconds that it takes for the memory wipe ability to recharge
-@export var flash_cooldown := 3.0
-var active_flash_cooldown := 3.0
+@export var dash_speed := 400.0
+var dashing := false
+var dash_direction : Vector2
+var dash_cooldown_active := false
+
 var flash_cooldown_active := false
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
+	if Input.is_action_just_pressed("left_click") && !dash_cooldown_active:
+		dash_direction = self.position.direction_to(get_global_mouse_position()).normalized()
+		dashing = true
+		$DashTimer.start()
+	
+	if dashing:
+		velocity = dash_direction * dash_speed
+	else:
+		velocity = direction * speed
 	
 	if $FlashAnimation.is_playing():
 		$Area2D/CollisionShape2D.disabled = false
@@ -29,16 +39,30 @@ func _process(delta):
 	else:
 		$AnimatedSprite2D.play("walk")
 	
+	# Handle mind wipe cooldown
 	if Input.is_action_just_pressed("space") && !flash_cooldown_active:
+		$MindWipeCooldown.start()
 		$FlashAnimation.visible = true
 		$FlashAnimation.play("flash")
 		flash_cooldown_active = true
-		active_flash_cooldown = 0  
-	if active_flash_cooldown < flash_cooldown && flash_cooldown_active:
-		active_flash_cooldown += delta
-		if active_flash_cooldown >= flash_cooldown:
-			flash_cooldown_active = false
 
 
 func _on_flash_animation_animation_finished():
 	$FlashAnimation.visible = false
+
+
+func _on_mind_wipe_cooldown_timeout():
+	flash_cooldown_active = false
+	$MindWipeCooldown.stop()
+
+
+func _on_dash_timer_timeout():
+	dashing = false
+	$DashTimer.stop()
+	$DashCooldown.start()
+	dash_cooldown_active = true
+
+
+func _on_dash_cooldown_timeout():
+	dash_cooldown_active = false
+	$DashCooldown.stop()
